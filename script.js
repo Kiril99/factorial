ef6e2040b21a4e49e64afe9859fc9b5157b90bb5
Kiril <kpanar82@gmail.com>
2018-02-09 13:27:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/*var i;
+for (var i = 0; i <= 99; i++) {
+		if (i%3==0 && i%2!==0) {
+			document.write(i + "<br>")
+		}
+		
+}
+var nuv = Number (prompt("Ведите число: "));
+var box =1;
+
+ 	for (var i = 1; i <= nuv; i++) {
+
+ 		box = box * i;
+ 		
+ 	}
+ 	document.write(box);*/
+
+var fact=Number (prompt("rfkeef"));
+var num =1;
+var box =1;
+	while(num <=fact){
+		box=box * num;
+		num++;
+	
+
+	}
+			document.write(box)
